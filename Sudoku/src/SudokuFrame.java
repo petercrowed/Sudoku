5f6315a2918bc4f6d5b868f9c3b3922fca673ec8
Peter <petercrowed@gmail.com>
2017-01-16 18:49:20 +0100

    Change-Id: I0000000000000000000000000000000000000000 Signed-off-by: Peter <petercrowed@gmail.com>

---
+++ Sudoku/src/SudokuFrame.java
@@ -0,0 +1,39 @@
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+public class SudokuFrame extends JFrame {
+
+	private SudokuPanel sPanel;
+
+	public SudokuFrame() {
+
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setTitle("Sudoku");
+		this.setMinimumSize(new Dimension(800, 600));
+
+		this.setLocationRelativeTo(null);
+
+		sPanel = new SudokuPanel();
+		JPanel windowPanel = new JPanel();
+		windowPanel.setLayout(new FlowLayout());
+		windowPanel.setPreferredSize(new Dimension(800, 600));
+		windowPanel.add(sPanel);
+
+		this.add(windowPanel);
+	}
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				SudokuFrame frame = new SudokuFrame();
+				frame.setVisible(true);
+			}
+		});
+	}
+
+}
